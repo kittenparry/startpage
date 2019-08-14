4e15f7254aff517cbc9aeee17361cc76b411c66a
tagged <contact@therealtagged.com>
2019-08-14 09:45:14 +0000

    initial commit

---
+++ scripts/links.js
@@ -0,0 +1,35 @@
+// input: [name]|[url]
+dict_services = {
+	'u': 'unraid|http://192.168.0.10/',
+	'p': 'plex|http://192.168.0.10:32400/web',
+	'n': 'nextcloud|https://192.168.0.10:444/',
+};
+
+// input: [url]
+dict_sites = {
+	'4': '4chan.org',
+	'd': 'drive.google.com',
+	'e': 'en.metal-tracker.com',
+	'g': 'github.com',
+	'gm': 'gmail.com',
+	'i': 'instagram.com',
+	'k': 'keep.google.com',
+	'r': 'reddit.com',
+	'ra': 'rarbg.to/torrents.php?category=2;4',
+	's': 'store.steampowered.com',
+	't': 'twitch.tv',
+	'tr': 'trakt.tv',
+	'y': 'youtube.com',
+};
+
+// input: [sub]
+dict_reddit = {
+	'2': '2meirl4meirl',
+	'c': 'coolgithubprojects',
+	'd': 'Dota2',
+	'da': 'DataHoarder',
+	'f': 'Fay_Suicide',
+	'k': 'KaylaErinCosplay',
+	's': 'suicidegirls',
+	'un': 'unixporn',
+};
